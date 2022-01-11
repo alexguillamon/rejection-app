a8939573ec4e8489ace4d91950dcb7410f2cfe26
AlexG <22422193+AlexGTalks@users.noreply.github.com>
2022-01-10 21:28:10 -0600

    implement load from local store

--- store/index.js
+++ store/index.js
@@ -1,11 +1,15 @@
 import { createStore } from "redux";
 import { rejectionReducer } from "../src/features/rejection-reducer";
+import { loadLocalStore, saveState } from "./localStorage-lib";
+import throttle from "lodash/throttle";
 
-const persistedState = []
+const persistedState = loadLocalStore();
 
 const store = createStore(
   rejectionReducer,
   persistedState
 )
+
+store.subscribe(throttle(() => saveState(store.getState()), 1000))
 
 export default store;
